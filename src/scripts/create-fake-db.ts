ad2090df8d64e5deb3647d61036ca3de2025c0c4
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-07 17:58:18 +0200

    fix

--- src/scripts/create-fake-db.ts
+++ src/scripts/create-fake-db.ts
@@ -1,8 +1,9 @@
 // Create a fake mongo db
 import 'dotenv/config'; // eslint-disable-line import/no-unassigned-import
+import process from 'node:process';
 
 function run() {
-	console.log('run');
+	console.log(`connect to: ${process?.env?.DB_URI ?? 'unknown'}`);
 }
 
 run();
