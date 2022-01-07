4e0e26f498a57738acabf0926bb6bec71c91b58a
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-07 20:25:14 +0200

    add nodemon and test

--- src/scripts/create-fake-db.ts
+++ src/scripts/create-fake-db.ts
@@ -1,10 +1,21 @@
 // Create a fake mongo db
 import 'dotenv/config'; // eslint-disable-line import/no-unassigned-import
+import mongoose from 'mongoose';
 import process from 'node:process';
 
-function run() {
-	console.log(`connect to: ${process.env.DB_URI ?? 'unknown'}`);
-}
+(async function () {
+	const connectionString = process.env.DB_URI;
 
-run();
+	if (connectionString) {
+		try {
+			console.info('connecting...');
+			const db = await mongoose.connect(connectionString);
+			console.log('connected!');
+			await db.connection.close();
+			console.info('disconnected');
+		} catch {
+			console.error('could not connect to db');
+		}
+	}
+})();
 
