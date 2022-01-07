425830a144c44d4884f49c62f5d1269a021af8a2
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-07 17:59:12 +0200

    remove

--- src/scripts/create-fake-db.ts
+++ src/scripts/create-fake-db.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'; // eslint-disable-line import/no-unassigned-import
 import process from 'node:process';
 
 function run() {
-	console.log(`connect to: ${process?.env?.DB_URI ?? 'unknown'}`);
+	console.log(`connect to: ${process.env.DB_URI ?? 'unknown'}`);
 }
 
 run();
