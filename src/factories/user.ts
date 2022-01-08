824c2817a51092c356899b7acd3037ab56e96cab
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-08 17:18:04 +0200

    age

--- src/factories/user.ts
+++ src/factories/user.ts
@@ -3,7 +3,7 @@ import {Factory} from 'rosie';
 
 const chance = Chance(); // eslint-disable-line new-cap
 
-export const userFactory = Factory.define('user').sequence('id').attr('firstName', () => chance.first()).attr('lastName', () => chance.last());
+export const userFactory = Factory.define('user').sequence('id').attr('firstName', () => chance.first()).attr('lastName', () => chance.last()).attr('age', () => chance.age());
 export interface User {
 	id: number;
 	firstName: string;
