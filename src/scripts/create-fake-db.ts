afb44220569a46c143d34582145ed7c59e0cc7a8
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-08 17:08:53 +0200

    insert users

--- src/scripts/create-fake-db.ts
+++ src/scripts/create-fake-db.ts
@@ -1,23 +1,28 @@
 // Create a fake mongo db
 import 'dotenv/config'; // eslint-disable-line import/no-unassigned-import
-import mongoose from 'mongoose';
 import process from 'node:process';
+import {MongoClient} from 'mongodb';
+import {userFactory} from '../factories/user';
+
+interface User {
+	id: number;
+	name: string;
+}
 
 async function run() {
 	const connectionString = process.env.DB_URI;
-
 	if (connectionString) {
+		const client = new MongoClient(connectionString);
 		try {
-			console.info(`connecting to: ${connectionString}`);
-			const db = await mongoose.connect(connectionString);
-			console.log('connected!');
-			await db.connection.close();
-			console.info('disconnected');
-		} catch {
-			console.error('could not connect to db');
+			await client.connect();
+			const db = client.db('test');
+			await db.dropDatabase();
+			const users = db.collection<User>('users');
+			await users.insertMany(userFactory.buildList(5));
+		} finally {
+			await client.close();
 		}
 	}
 }
 
 void run();
-
---
+++ xo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	rules: {
+		'import/extensions': 0,
+		'unicorn/prefer-module': 0,
+	},
+};
