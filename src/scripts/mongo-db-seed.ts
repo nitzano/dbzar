daccdfd3e7198a951be0d0127ae7ff896bab9eb5
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-08 17:30:56 +0200

    reduce to 10

--- src/scripts/mongo-db-seed.ts
+++ src/scripts/mongo-db-seed.ts
@@ -14,7 +14,7 @@ async function run() {
 			await db.dropDatabase();
 			// User
 			const users = db.collection<User>('users');
-			await users.insertMany(userFactory.buildList(50));
+			await users.insertMany(userFactory.buildList(10));
 		} catch (error: unknown) {
 			console.error(error);
 		} finally {
