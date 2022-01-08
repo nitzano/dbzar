19cc8ad2f43f6db2bf76a54e66b3741123c07e2e
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-08 16:47:00 +0200

    disable

--- src/factories/user.ts
+++ src/factories/user.ts
@@ -1,6 +1,8 @@
-import {Chance as chance} from 'chance';
+import {Chance} from 'chance';
 import {Factory} from 'rosie';
 
-export const userFactory = Factory.define('user').sequence('id').attr('name', () => chance().name());
+const chance = Chance(); // eslint-disable-line new-cap
+
+export const userFactory = Factory.define('user').sequence('id').attr('name', () => chance.name());
 
 console.log(userFactory.buildList(5));
