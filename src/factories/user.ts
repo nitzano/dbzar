cb78ab8691738438431a7b8ae9636cceb4ba329b
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-08 17:22:37 +0200

    remove id

--- src/factories/user.ts
+++ src/factories/user.ts
@@ -3,9 +3,8 @@ import {Factory} from 'rosie';
 
 const chance = Chance(); // eslint-disable-line new-cap
 
-export const userFactory = Factory.define('user').sequence('id').attr('firstName', () => chance.first()).attr('lastName', () => chance.last()).attr('age', () => chance.age());
+export const userFactory = Factory.define('user').attr('firstName', () => chance.first()).attr('lastName', () => chance.last()).attr('age', () => chance.age());
 export interface User {
-	id: number;
 	firstName: string;
 	lastName: string;
 	age: number;
