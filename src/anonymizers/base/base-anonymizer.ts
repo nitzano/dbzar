8df581f12935a78687c9b4ba017ffc428be1ce2d
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-16 14:30:33 +0200

    add base

--- src/anonymizers/base/base-anonymizer.ts
+++ src/anonymizers/base/base-anonymizer.ts
@@ -1,11 +1,28 @@
+type InputValue = number | string | boolean;
+
 export class BaseAnonymizer {
-	constructor(private readonly config: any) {}
+	anonymize(value: InputValue): InputValue {
+		switch (typeof (value)) {
+			case 'string':
+				return this.anonymizeString(value);
+			case 'number':
+				return this.anonymizeNumber(value);
+			case 'boolean':
+				return this.anonymizeBoolean(value);
+			default:
+				return value;
+		}
+	}
 
 	anonymizeString(value: string): string {
 		return value;
 	}
 
 	anonymizeNumber(value: number): number {
+		return value;
+	}
+
+	anonymizeBoolean(value: boolean): boolean {
 		return value;
 	}
 }
