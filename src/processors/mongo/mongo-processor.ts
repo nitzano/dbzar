7da1e24f3e5c94b94171a613e2c26b02f2278abe
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-16 22:39:53 +0200

    connect to mongo

--- src/processors/mongo/mongo-processor.ts
+++ src/processors/mongo/mongo-processor.ts
@@ -1,24 +1,34 @@
 import {MongoClient} from 'mongodb';
-import {Config} from '../../config/types';
+import {CollectionConfig, Config} from '../../config/types';
 
 export class MongoProcessor {
-	constructor(private readonly config: Config, private readonly uri: string) {}
+	private readonly client: MongoClient;
+
+	constructor(private readonly config: Config, private readonly uri: string) {
+		this.client = new MongoClient(this.uri);
+	}
 
 	async processDb(dbName: string): Promise<void> {
-		const client = new MongoClient(this.uri);
-
 		try {
-			await client.connect();
+			await this.client.connect();
 			console.log('Connected to server');
 
-			const db = client.db(dbName);
+			const db = this.client.db(dbName);
 
 			const collections = db.listCollections();
 			console.log(`collections = ${JSON.stringify(collections.map(({name}) => name))}`);
 		} catch (error: unknown) {
 			console.error(error);
 		} finally {
-			await client.close();
+			await this.client.close();
 		}
+	}
+
+	async processCollection(collectionName: string, collectionConfig: CollectionConfig) {
+		// Get the collection from the config
+		console.log(collectionName);
+		console.log(collectionConfig.name);
+
+		// Process every document
 	}
 }
