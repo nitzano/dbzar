c19c8e249b2dfbcfd1f25185feda9fe939bbaf63
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-16 11:16:59 +0200

    added mask

---
+++ src/anonymizers/mask/mask-anonymizer.ts
@@ -0,0 +1,11 @@
+
+export interface MaskOptions {
+	character: string;
+	exclude?: string;
+	excludeEnd?: number;
+	excludeStart?: number;
+}
+
+export const defaultMaskConfig: MaskOptions = {
+	character: '*',
+};
