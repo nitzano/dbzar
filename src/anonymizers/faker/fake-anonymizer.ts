9785705c71a77d125a17d3ea2fc9563e8d9d98e4
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-16 14:25:09 +0200

    build fake

--- src/anonymizers/faker/fake-anonymizer.ts
+++ src/anonymizers/faker/fake-anonymizer.ts
@@ -1,3 +1,26 @@
 import {BaseAnonymizer} from '../base/base-anonymizer';
+import {FakeOptions} from './fake-options';
 
-export class FakeAnonymizer extends BaseAnonymizer {}
+export class FakeAnonymizer extends BaseAnonymizer {
+	constructor(private readonly options: FakeOptions) {
+		super();
+	}
+
+	anonymizeString(value: string): string {
+		switch (this.options.fakeValue) {
+			case 'firstName':
+			case 'first':
+				return 'firstName';
+
+			case 'lastName':
+			case 'last':
+				return 'lastName';
+			case 'age':
+				return 'age';
+			default:
+				break;
+		}
+
+		return value;
+	}
+}
