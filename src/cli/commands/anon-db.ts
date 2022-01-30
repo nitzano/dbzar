e6eb78d911c0a8849f45c06f190632345d8031bc
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-30 15:51:28 +0200

    chore: remove db code

--- src/cli/commands/anon-db.ts
+++ src/cli/commands/anon-db.ts
@@ -1,11 +1,1 @@
-import process from 'node:process';
-import {Command} from 'commander';
-
-const program = new Command();
-
-program
-	.version('0.0.1')
-	.option('-uri, --connection-uri', 'db connection string')
-	.option('-db, --db-name', 'database name');
-
-program.parse(process.argv);
+// Noop
