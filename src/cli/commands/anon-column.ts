f9d8afc41bf94caa06a339c0caa7b8f25df0c21b
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-30 15:52:14 +0200

    chore: support postgres as well

--- src/cli/commands/anon-column.ts
+++ src/cli/commands/anon-column.ts
@@ -1,6 +1,7 @@
 import {EngineType} from '../../config/types';
 import {Processor} from '../../processors/base-processor/base-processor';
 import {MongoProcessor} from '../../processors/mongo/mongo-processor';
+import {PostgresProcessor} from '../../processors/postgres/postgres-processor';
 import {ProviderType} from '../../types/types';
 import {getConnectionStringEngine} from '../../utils/get-connection-string-engine';
 
@@ -18,6 +19,9 @@ export async function anonymizeColumn(
 		switch (engine) {
 			case EngineType.Mongo:
 				processor = new MongoProcessor(connectionStringUri);
+				break;
+			case EngineType.PostGres:
+				processor = new PostgresProcessor(connectionStringUri);
 				break;
 			default:
 				break;
