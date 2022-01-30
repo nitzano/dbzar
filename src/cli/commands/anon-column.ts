4825b5e6dd19027545438f41360b85f37046bd8b
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-01-30 15:51:08 +0200

    chore: base anon

---
+++ src/cli/commands/anon-column.ts
@@ -0,0 +1,30 @@
+import {EngineType} from '../../config/types';
+import {Processor} from '../../processors/base-processor/base-processor';
+import {MongoProcessor} from '../../processors/mongo/mongo-processor';
+import {ProviderType} from '../../types/types';
+import {getConnectionStringEngine} from '../../utils/get-connection-string-engine';
+
+export async function anonymizeColumn(
+	connectionStringUri: string,
+	tableName: string,
+	columnName: string,
+	provider: ProviderType,
+) {
+	// Parse the engine
+	const engine = getConnectionStringEngine(connectionStringUri);
+	let processor: Processor | undefined;
+
+	if (engine) {
+		switch (engine) {
+			case EngineType.Mongo:
+				processor = new MongoProcessor(connectionStringUri);
+				break;
+			default:
+				break;
+		}
+
+		if (processor) {
+			await processor.processColumn(tableName, columnName, provider);
+		}
+	}
+}
