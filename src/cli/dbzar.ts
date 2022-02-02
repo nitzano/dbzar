49e9edbada1049b33d99b5f3edec25b5a372ea62
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-02 22:59:49 +0200

    chore: write as function

--- src/cli/dbzar.ts
+++ src/cli/dbzar.ts
@@ -7,6 +7,32 @@ import {anonymizeColumn} from './commands/anon-column';
 
 const program = new Command();
 
+async function runAction(
+	uri: string,
+	db: string,
+	table: string,
+	column: string,
+	options: any,
+) {
+	try {
+		const answer = await prompt<{run: boolean}>({
+			skip: options.skipConfirm === true,
+			type: 'confirm',
+			name: 'run',
+			message: `Are you sure you want to anonymize column "${column}" in "${db}/${table}" `,
+			initial: 'true',
+		});
+		if (!answer.run) {
+			return;
+		}
+	} catch (error: unknown) {
+		console.error(error);
+		throw new Error('could not receive answer');
+	}
+
+	await anonymizeColumn(uri, table, column, options.provider, db);
+}
+
 // Anon column
 program
 	.command('anon-col')
@@ -17,26 +43,6 @@ program
 	.option('-p --provider <provider>', 'provider to be used for column', 'mask')
 	.option('-skip --skip-confirm', 'skip confirmation')
 	.description('anonymize a single column in a table')
-	.action(
-		async (uri: string, db: string, table: string, column: string, options) => {
-			try {
-				const answer = await prompt<{run: boolean}>({
-					skip: options.skipConfirm === true,
-					type: 'confirm',
-					name: 'run',
-					message: `Are you sure you want to anonymize column "${column}" in "${db}/${table}" `,
-					initial: 'true',
-				});
-				if (!answer.run) {
-					return;
-				}
-			} catch (error: unknown) {
-				console.error(error);
-				throw new Error('could not receive answer');
-			}
-
-			await anonymizeColumn(uri, table, column, options.provider, db);
-		},
-	);
+	.action(runAction);
 
 program.parse(process.argv);
