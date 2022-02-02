765861a11173c1823dfbea11c361e222f3f15c6e
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-02 22:57:29 +0200

    chore: added builder

--- src/cli/dbzar.ts
+++ src/cli/dbzar.ts
@@ -2,6 +2,7 @@
 
 import process from 'node:process';
 import {Command} from 'commander';
+import {prompt} from 'enquirer';
 import {anonymizeColumn} from './commands/anon-column';
 
 const program = new Command();
@@ -14,9 +15,28 @@ program
 	.argument('<table>', 'table name')
 	.argument('<column>', 'column name')
 	.option('-p --provider <provider>', 'provider to be used for column', 'mask')
+	.option('-skip --skip-confirm', 'skip confirmation')
 	.description('anonymize a single column in a table')
-	.action(async (uri, db, table, column, options) => {
-		await anonymizeColumn(uri, table, column, options.provider, db);
-	});
+	.action(
+		async (uri: string, db: string, table: string, column: string, options) => {
+			try {
+				const answer = await prompt<{run: boolean}>({
+					skip: options.skipConfirm === true,
+					type: 'confirm',
+					name: 'run',
+					message: `Are you sure you want to anonymize column "${column}" in "${db}/${table}" `,
+					initial: 'true',
+				});
+				if (!answer.run) {
+					return;
+				}
+			} catch (error: unknown) {
+				console.error(error);
+				throw new Error('could not receive answer');
+			}
+
+			await anonymizeColumn(uri, table, column, options.provider, db);
+		},
+	);
 
 program.parse(process.argv);
