007be135e04ae19fec72219514e1928240d0a3f6
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-06 22:43:12 +0200

    chore: remove unused imports

--- xo.config.js
+++ xo.config.js
@@ -1,12 +1,14 @@
 module.exports = {
+	plugins: ['unused-imports'],
 	rules: {
 		'import/extensions': 0,
 		'unicorn/prefer-module': 0,
 		'unicorn/no-empty-file': 0,
 		'unicorn/prefer-node-protocol': 0,
 		'no-console': [1, {allow: ['debug', 'warn', 'error']}],
+		'no-unused-vars': 'error',
+		'unused-imports/no-unused-imports': 'error',
 	},
-
 	globals: {
 		__MONGO_URI__: true,
 	},
