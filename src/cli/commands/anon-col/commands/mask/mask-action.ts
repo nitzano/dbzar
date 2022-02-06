d24912d877c1cbaaaaa32b8508146f588569679e
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-06 18:19:37 +0200

    chore: complete mask action

--- src/cli/commands/anon-col/commands/mask/mask-action.ts
+++ src/cli/commands/anon-col/commands/mask/mask-action.ts
@@ -1,5 +1,27 @@
 import {Command} from 'commander';
+import {MaskAnonymizer} from '../../../../../anonymizers/mask/mask-anonymizer';
+import {
+	defaultMaskOptions,
+	MaskOptions,
+} from '../../../../../anonymizers/mask/mask-options';
+import {processColumn} from '../../anon-col/action/process-column';
 
-export function maskAction(this: Command) {
-	console.log(this.name());
+export async function maskAction(this: Command) {
+	const maskOptions: MaskOptions = {
+		...defaultMaskOptions,
+	};
+
+	const [connectionString, dbName, tableName, columnName] = this.args;
+
+	// Build anonymizer
+	const anonymizer: MaskAnonymizer = new MaskAnonymizer(maskOptions);
+
+	// Anonymize column
+	await processColumn(
+		connectionString,
+		anonymizer,
+		tableName,
+		columnName,
+		dbName,
+	);
 }
