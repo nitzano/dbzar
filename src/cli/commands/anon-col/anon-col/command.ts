bd3c326f8fee053d4cf266d51dcbd19a4f9d6a96
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-06 18:28:49 +0200

    chore: remove old files

--- src/cli/commands/anon-col/anon-col/command.ts
+++
@@ -1,9 +0,0 @@
-import {Command} from 'commander';
-import {maskCommand} from '../commands/mask/mask-command';
-
-export const anonColCommand = new Command('anon-col');
-
-anonColCommand
-	.description('anonymize a single column in a table')
-	.addCommand(maskCommand, {isDefault: true})
-	.option('-C --no-confirm', 'Do not wait for user confirmation');
