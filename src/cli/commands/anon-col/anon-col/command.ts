a9d1e74b18760a22573675f5a89d583a77b386c9
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-06 18:19:55 +0200

    chore: remove arguments from mask command

--- src/cli/commands/anon-col/anon-col/command.ts
+++ src/cli/commands/anon-col/anon-col/command.ts
@@ -1,13 +1,9 @@
 import {Command} from 'commander';
-import {maskCommand} from '../commands/mask/command';
+import {maskCommand} from '../commands/mask/mask-command';
 
 export const anonColCommand = new Command('anon-col');
 
 anonColCommand
 	.description('anonymize a single column in a table')
-	.argument('<uri>', 'connection string')
-	.argument('<db>', 'database name')
-	.argument('<table>', 'table name')
-	.argument('<column>', 'column name')
 	.addCommand(maskCommand, {isDefault: true})
 	.option('-C --no-confirm', 'Do not wait for user confirmation');
