f96f8bb26c1de3063f70f60170857240955ecb14
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-11 23:39:28 +0200

    test: adding more

--- src/config/utils/__tests__/validate-config.spec.ts
+++ src/config/utils/__tests__/validate-config.spec.ts
@@ -1,12 +1,33 @@
 import {validateConfig} from '../validate-config';
-import validConfig from './fixtures/valid-config.json';
 
 describe('validate-config', () => {
 	it('should allow to pass an valid config', () => {
+		const validConfig: any = {
+			tables: [
+				{
+					name: 'table1',
+					columns: [
+						{
+							name: 'column1',
+							provider: 'mask',
+						},
+					],
+				},
+			],
+		};
+
 		const isValid: boolean = validateConfig(validConfig);
 		expect(isValid).toBe(true);
 	});
 	it('should allow to revoke an invalid config', () => {
+		const invalidConfig: any = {
+			tables: [],
+		};
+
+		const isValid: boolean = validateConfig(invalidConfig);
+		expect(isValid).toBe(false);
+	});
+	it('should allow to fix  an invalid config', () => {
 		const invalidConfig: any = {
 			tables: [],
 		};
