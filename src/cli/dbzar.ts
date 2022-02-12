9f4be937324e4ee353ff8b7a4522dbf509d0ee90
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-12 15:06:04 +0200

    chore: bind

--- src/cli/dbzar.ts
+++ src/cli/dbzar.ts
@@ -3,10 +3,11 @@
 import process from 'node:process';
 import {Command} from 'commander';
 import {anonColCommand} from './commands/anon-col/anon-col';
+import {anonDbCommand} from './commands/anon-db/anon-db-command';
 
 const program = new Command();
 
 // Anon column
 program.addCommand(anonColCommand);
-
+program.addCommand(anonDbCommand);
 program.parse(process.argv);
