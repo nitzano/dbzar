01a36df107fbd193f50b2494f0dc99f3cccb8441
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-12 15:15:09 +0200

    chore: fix cli

--- src/cli/dbzar.ts
+++ src/cli/dbzar.ts
@@ -2,8 +2,11 @@
 
 import process from 'node:process';
 import {Command} from 'commander';
+import dotenv from 'dotenv';
 import {anonColCommand} from './commands/anon-col/anon-col';
 import {anonDbCommand} from './commands/anon-db/anon-db-command';
+
+dotenv.config();
 
 const program = new Command();
 
