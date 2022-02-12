22ff6494f80cf7dba4af18ed2408bcbc9a27c3b2
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-12 18:43:07 +0200

    chore: show logs

--- src/cli/commands/anon-db/utils/load-dbzar-config.ts
+++ src/cli/commands/anon-db/utils/load-dbzar-config.ts
@@ -1,9 +1,12 @@
 import {cosmiconfig} from 'cosmiconfig';
 import {Config} from '../../../../config/types';
 import {validateConfig} from '../../../../config/utils/validate-config';
+import {createLogger} from '../../../../services/loggers/debug-logger';
 
 const moduleName = 'dbzar';
 const explorer = cosmiconfig(moduleName);
+
+const logger = createLogger(__filename);
 
 export async function loadDbzarConfig(): Promise<Config | null> {
 	const configResult = await explorer.search();
@@ -20,9 +23,12 @@ export async function loadDbzarConfig(): Promise<Config | null> {
 			if (configData) {
 				try {
 					// TODO: not checking correctly
+					logger(`processing ${JSON.stringify(configData, null, 2)}`);
 					validateConfig(configData);
+					logger('valid');
 					return configData as Config;
 				} catch (error: unknown) {
+					logger('caught!');
 					console.error(error);
 					return null;
 				}
