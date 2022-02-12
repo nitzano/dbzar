81f6367f3bcb38f19e8d074296521e38433fe248
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-12 12:12:26 +0200

    chore: broken

--- src/config/utils/validate-config.ts
+++ src/config/utils/validate-config.ts
@@ -1,13 +1,17 @@
-import Ajv, {ValidateFunction} from 'ajv';
+import Ajv, {JSONSchemaType, ValidateFunction} from 'ajv';
 import configSchema from '../assets/config.schema.json';
+import {Config} from '../types';
 
 let ajv: Ajv;
 let validate: ValidateFunction;
 
+console.log(typeof configSchema);
+
 export function validateConfig(data: any): boolean {
 	if (!ajv) {
+		const schema: JSONSchemaType<Config> = configSchema;
 		ajv = new Ajv();
-		validate = ajv.compile(configSchema);
+		validate = ajv.compile(schema);
 	}
 
 	return validate(data);
