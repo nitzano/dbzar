085af84a6f60ce7d7859577c71f700995bfff320
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-12 12:19:20 +0200

    chore: revert

--- src/config/utils/validate-config.ts
+++ src/config/utils/validate-config.ts
@@ -1,17 +1,13 @@
-import Ajv, {JSONSchemaType, ValidateFunction} from 'ajv';
+import Ajv, {ValidateFunction} from 'ajv';
 import configSchema from '../assets/config.schema.json';
-import {Config} from '../types';
 
 let ajv: Ajv;
 let validate: ValidateFunction;
 
-console.log(typeof configSchema);
-
 export function validateConfig(data: any): boolean {
 	if (!ajv) {
-		const schema: JSONSchemaType<Config> = configSchema;
 		ajv = new Ajv();
-		validate = ajv.compile(schema);
+		validate = ajv.compile(configSchema);
 	}
 
 	return validate(data);
