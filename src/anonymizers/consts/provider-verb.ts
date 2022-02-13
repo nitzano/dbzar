64ba4a62187bcf104a572b69ad66afe9e6b3c298
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-13 10:56:05 +0200

    chore: add provider verb

--- src/anonymizers/consts/provider-verb.ts
+++ src/anonymizers/consts/provider-verb.ts
@@ -1,1 +1,9 @@
-// Noop
+import {ProviderType} from '../../types/types';
+
+export const providerVerb: Record<ProviderType, string> = {
+	mask: 'masking',
+	fake: 'faking',
+	scramble: 'scrambling',
+	const: 'turning into const',
+	remove: 'removing',
+};
