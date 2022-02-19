ec1961e318e934d86da58b393edb0499b0a29616
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-19 11:18:51 +0200

    chore: allow to process column

---
+++ src/api/anon-column.ts
@@ -0,0 +1,50 @@
+import {Anonymizer} from '../anonymizers/types';
+import {createAnonymizer} from '../anonymizers/utils/create-anonymizer';
+import {getProcessor} from '../cli/commands/anon-col/helpers/get-processor';
+import {Processor} from '../processors/base-processor/processor';
+import {createLogger} from '../services/loggers/debug-logger';
+import {Provider} from '../types/types';
+
+const logger = createLogger(__filename);
+
+/**
+ *
+ *
+ * @export
+ * @param {string} uri - connection string
+ * @param {string} dbName  - database name
+ * @param {string} tableName  - table name
+ * @param {string} columnName - column name
+ * @param {Provider} provider - provider to process column with
+ * @return {*}  {Promise<void>}
+ */
+export async function anonColumn(
+	uri: string,
+	dbName: string,
+	tableName: string,
+	columnName: string,
+	provider: Provider,
+): Promise<void> {
+	if (!uri) {
+		throw new Error('No uri in configuration');
+	}
+
+	const processor: Processor | undefined = getProcessor(uri);
+
+	logger(`processing ${dbName}`);
+	if (processor) {
+		const anonymizer: Anonymizer = createAnonymizer(
+			provider.type,
+			provider.options,
+		);
+
+		if (anonymizer) {
+			await processor.processColumn(tableName, columnName, anonymizer, dbName);
+			return;
+		}
+
+		throw new Error('could not find anonymizer for the provider');
+	}
+
+	throw new Error(`could not detect processor for uri`);
+}
