20bf793c9fa7376e1333b2a13563787e7e93089e
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-19 10:24:39 +0200

    feat: allow to anonymize databases with api (#87)
    * chore: skeleton
    
    * chore: add promises
    
    * chore: insert index
    
    * chore: remove comment
    
    * chore: insert debug
    
    * fix: typo

---
+++ src/api/anon-db.ts
@@ -0,0 +1,37 @@
+import {getProcessor} from '../cli/commands/anon-col/helpers/get-processor';
+import {Config} from '../config/types';
+import {Processor} from '../processors/base-processor/processor';
+import {getCollections} from '../processors/utils/get-collections';
+import {createLogger} from '../services/loggers/debug-logger';
+
+const logger = createLogger(__filename);
+
+/**
+ * Anonymize entire database
+ *
+ * @export
+ * @param {Config} config
+ * @return {*}  {Promise<void>}
+ */
+export async function anonDb(config: Config): Promise<void> {
+	const {uri, dbName} = config;
+
+	if (!uri) {
+		throw new Error('No uri in configuration');
+	}
+
+	const processor: Processor | undefined = getProcessor(uri);
+
+	logger(`processing ${dbName}`);
+	if (processor) {
+		const collections = getCollections(config);
+		logger(`processing ${collections?.length} collections`);
+		const promises = collections.map(
+			async ({tableName, columnName, anonymizer}) =>
+				processor.processColumn(tableName, columnName, anonymizer, dbName),
+		);
+		await Promise.all(promises);
+	}
+
+	throw new Error(`could not detect processor for uri`);
+}
