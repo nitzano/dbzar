0cf23f991395690f6085875a9693cf86914a7219
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-24 06:57:38 +0200

    chore(cli): extract as value

--- src/cli/commands/anon-col/helpers/add-connection-options.ts
+++ src/cli/commands/anon-col/helpers/add-connection-options.ts
@@ -2,8 +2,8 @@ import {Command} from 'commander';
 
 export function addConnectionOptions(cmd: Command): Command {
 	return cmd
-		.requiredOption('-u --uri', 'Connection string')
-		.requiredOption('-db --database', 'Database name')
-		.requiredOption('-t --table', 'Table name')
-		.requiredOption('-c --column', 'Column name');
+		.requiredOption('-u --uri <connectionString>', 'Connection string')
+		.requiredOption('-db --database <databaseName>', 'Database name')
+		.requiredOption('-t --table <tableName>', 'Table name')
+		.requiredOption('-c --column <columnName>', 'Column name');
 }
