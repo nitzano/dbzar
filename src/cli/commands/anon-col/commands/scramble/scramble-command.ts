bd848cd80fe420e03a252112dc36560c17e225ff
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-24 07:01:43 +0200

    chore: remove specific from scramble

--- src/cli/commands/anon-col/commands/scramble/scramble-command.ts
+++ src/cli/commands/anon-col/commands/scramble/scramble-command.ts
@@ -1,9 +1,6 @@
 import {Command} from 'commander';
-import {addConnectionOptions} from '../../helpers/add-connection-options';
 import {scrambleAction} from './scramble-action';
 
 export const scrambleCommand = new Command('scramble');
 
 scrambleCommand.description('scramble a single column').action(scrambleAction);
-
-addConnectionOptions(scrambleCommand);
