6e219bd0deac546e89e04850d08ad59df8506f64
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-02-24 08:26:17 +0200

    docs: cli anon-db

--- src/cli/commands/anon-db/anon-db-command.ts
+++ src/cli/commands/anon-db/anon-db-command.ts
@@ -4,6 +4,6 @@ import {anonDbAction} from './anon-db-action';
 export const anonDbCommand = new Command('anon-db');
 
 anonDbCommand
-	.description('anonymize an entire database')
+	.description('Anonymize an entire database')
 	.argument('[uri]', 'connection string')
 	.action(anonDbAction);
