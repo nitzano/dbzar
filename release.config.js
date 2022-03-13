df92b65d1b7cb6976358c7b64a6d71d8686e9409
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2022-03-13 17:24:44 +0200

    build: semantic build

--- release.config.js
+++ release.config.js
@@ -1,8 +1,17 @@
+const DOCKER_IMAGE_NAME = 'nitzano/dbzar';
+
 module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
 		'@semantic-release/npm',
 		'@semantic-release/github',
+		[
+			'@semantic-release/exec',
+			{
+				prepareCmd: `docker build -t ${DOCKER_IMAGE_NAME} .`,
+			},
+		],
+		['semantic-release-docker', {name: DOCKER_IMAGE_NAME}],
 	],
 };
