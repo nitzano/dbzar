545a93db2484de596cb5775ce0ea7d4bc1dae51f
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2023-11-17 13:38:35 +0200

    chore: rename folders

--- src/cli/commands/anon-db/process-db.ts
+++ src/cli/commands/anon-db/process-db.ts
@@ -3,7 +3,7 @@ import ora from 'ora';
 import {providerEmoji} from '../../../anonymizers/consts/provider-emoji';
 import {providerVerb} from '../../../anonymizers/consts/provider-verb';
 import {type Processor} from '../../../processors/base-processor/processor';
-import {type Collection} from '../../../processors/databases/types/collection';
+import {type Collection} from '../../../processors/db/types/collection';
 import {type ProviderType} from '../../../types/types';
 
 async function processCollection(
